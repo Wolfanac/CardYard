50b3a3b4e1a678f096986bdc936506b4b47b4d14
Silvric <delaplaceric@gmail.com>
2025-04-20 13:13:10 +0200

    cards_attribution.c

--- cards_attribution.c
+++ cards_attribution.c
@@ -27,86 +27,86 @@ FILE* cards_atribution(){
         }
         card=rand()%15;
     }
-    return usedcards;
+    fclose(usedcards);
+    return 0;
+    
 }
 
 
-// int cards_change(){
-//     FILE* usedcards = NULL;
-//     usedcards = fopen("cards.txt", "r");
+ int change_cards(){ 
+     FILE* usedcards = NULL;
+     usedcards = fopen("cards.txt", "r");
+     //check if the file have been open
+     if (usedcards == NULL) {
+         printf("\n ouverture impossible");
+         exit(1);
+     }
+     rewind(usedcards);
 
-//     //check if the file have been open
-//     if (usedcards == NULL) {
-//         printf("\n ouverture impossible");
-//         exit(1);
-//     }
-//     rewind(usedcards);
+     //make the player change the values in the pile of cards
+     int a=0;
+     char  value[10];
+     int i=0, number=0, v=0, fileupdate[140], changenumber, originalnumber;
+     printf("\n do you wish to change some cards ? ");
+     scanf("%s", value);
 
-//     //make the player change the values in the pile of cards
-//     int a=0;
-//     char  value[10];
-//     int i=0, number=0, v=0, fileupdate[140], changenumber, originalnumber;
-//     printf("\n do you wish to change some cards ? ");
-//     scanf("%s", value);
+     while (strcmp(value, "no") != 0){
 
-//     while (strcmp(value, "no") != 0){
+         while (strcmp(value, "yes") == 0) {
 
-//         while (strcmp(value, "yes") == 0) {
+             i = 0;
+             v = 0;
+             number = 0;
 
-//             i = 0;
-//             v = 0;
-//             number = 0;
+             printf("\n what numbers do you wish to change ?");
+             scanf("%d", &originalnumber);
+             printf("\n to what number ?");
+             scanf("%d", &changenumber);
+             freopen("cards.txt", "r", usedcards);
 
-//             printf("\n what numbers do you wish to change ?");
-//             scanf("%d", &originalnumber);
-//             printf("\n to what number ?");
-//             scanf("%d", &changenumber);
-//             freopen("cards.txt", "r", usedcards);
+             //put all values in a list
+             while (fscanf(usedcards, "%d", &a)==1){
+                 fileupdate[i]=a;
+                 i++;
+             }
 
-//             //put all values in a list
-//             while (fscanf(usedcards, "%d", &a)==1){
-//                 fileupdate[i]=a;
-//                 i++;
-//             }
+             //change the values in the list
+             while (v<i){
+                 if (fileupdate[v]==originalnumber){
+                     fileupdate[v]=changenumber;
+                     number++;
+                 }
+                 v++;
+             }
+             if (number==0){
+                 printf("no %d number found", originalnumber);
+             }
+             rewind(usedcards);
+             v=0;
+             freopen("cards.txt", "w", usedcards);
 
-//             //change the values in the list
-//             while (v<i){
-//                 if (fileupdate[v]==originalnumber){
-//                     fileupdate[v]=changenumber;
-//                     number++;
-//                 }
-//                 v++;
-//             }
-//             if (number==0){
-//                 printf("no %d number found", originalnumber);
-//             }
-//             rewind(usedcards);
-//             v=0;
-//             freopen("cards.txt", "w", usedcards);
+             //print the list in the file
+             while (v<i){
+                 fprintf(usedcards, "%d", fileupdate[v]);
+                 fputs(" ", usedcards);
+                 v++;
+             }
 
-//             //print the list in the file
-//             while (v<i){
-//                 fprintf(usedcards, "%d", fileupdate[v]);
-//                 fputs(" ", usedcards);
-//                 v++;
-//             }
+             //ask to continue
+             printf("\ndo you wish to change some cards ? ");
+             scanf("%s", value);
 
-//             //ask to continue
-//             printf("\ndo you wish to change some cards ? ");
-//             scanf("%s", value);
+         }
 
-//         }
+         //mistypes
+         if (strcmp(value, "no") != 0 && strcmp(value, "yes") != 0) {
+             printf("\nPlease enter either 'yes' or 'no': ");
+             scanf("%s", value);
+         }
 
-//         //mistypes
-//         if (strcmp(value, "no") != 0 && strcmp(value, "yes") != 0) {
-//             printf("\nPlease enter either 'yes' or 'no': ");
-//             scanf("%s", value);
-//         }
-
-//     }
-
+     }
 
 
-//     fclose(usedcards);
-//     return 0;
-// }
+     fclose(usedcards);
+     return 0;
+ }
