6e166f6159c2e6e1b6b0877ccafcba2186129e21
Kkirbo <aymaneamara935@gmail.com>
2025-04-23 02:00:29 +0200

    principal_deck_load.c

--- principal_deck_load.c
+++ principal_deck_load.c
@@ -1,12 +1,18 @@
-int* principale_deck_load(FILE *fichier, int n) {
+
+int* principale_deck_load(FILE *f, int n) {
+    FILE* f=fopen("cards.txt","r+");
+     if (f == NULL) {
+        printf("Error opening cards.txt\n");
+        return;
+    }
     int *pioche_principale = calloc(n, sizeof(int));
     if (pioche_principale == NULL) {
-        printf("Erreur d'allocation mémoire.\n");
+        printf("Memory allocation issue\n");
         return NULL;
     }
 
     int i = 0;
-    while (fscanf(fichier, "%d", &pioche_principale[i]) == 1 && i < n) {
+    while (fscanf(f, "%d", &pioche_principale[i]) == 1 && i < n) {
         i++;
     }
 
