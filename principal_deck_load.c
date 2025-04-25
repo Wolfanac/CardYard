94449365eae93aec229144770ecf23d5ba06ba9a
Kkirbo <aymaneamara935@gmail.com>
2025-04-26 01:49:41 +0200

    Update principal_deck_load.c

--- principal_deck_load.c
+++ principal_deck_load.c
@@ -1,10 +1,13 @@
 
-int* principale_deck_load(FILE *f, int n) {
-    FILE* f=fopen("cards.txt","r+");
-     if (f == NULL) {
-        printf("Error opening cards.txt\n");
-        return;
+#include <stdio.h>
+#include <stdlib.h>
+
+int* principale_deck_load(FILE *input_file, int n) {
+    if (input_file == NULL) {
+        printf("Error: input file is NULL\n");
+        return NULL;
     }
+
     int *pioche_principale = calloc(n, sizeof(int));
     if (pioche_principale == NULL) {
         printf("Memory allocation issue\n");
@@ -12,7 +15,8 @@ int* principale_deck_load(FILE *f, int n) {
     }
 
     int i = 0;
-    while (fscanf(f, "%d", &pioche_principale[i]) == 1 && i < n) {
+    rewind(input_file);
+    while (fscanf(input_file, "%d", &pioche_principale[i]) == 1 && i < n) {
         i++;
     }
 
