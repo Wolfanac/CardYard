ba9901d0a2612f9b572784858fdaff4732e352c3
Kkirbo <aymaneamara935@gmail.com>
2025-04-26 01:45:35 +0200

    Update principal_deck_save.c

--- principal_deck_save.c
+++ principal_deck_save.c
@@ -1,11 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 void principale_deck_save(FILE *fichier, FILE *register_zone, int number_of_cards) {
-    int *principal_deck = calloc(n, sizeof(int));
+    int *principal_deck = calloc(number_of_cards, sizeof(int));
+    if (principal_deck == NULL) {
+        printf("Memory allocation error\n");
+        return;
+    }
     int i = 0, a;
-    while (fscanf(fichier, "%d", &a) == 1 && i < n) {
-        pioche_principale[i] = a;
-        fprintf(zone_enregistrement, "%d\n", pioche_principale[i]);
+    rewind(fichier);
+    while (fscanf(fichier, "%d", &a) == 1 && i < number_of_cards) {
+        principal_deck[i] = a;
+        fprintf(register_zone, "%d\n", principal_deck[i]);
         i++;
     }
 
-    free(pioche_principale);
+    free(principal_deck);
 }
