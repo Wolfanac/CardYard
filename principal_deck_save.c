23f67d731d9e1f4469b94f875ed75bf26b96af05
Kkirbo <aymaneamara935@gmail.com>
2025-04-26 02:44:47 +0200

    Update principal_deck_save.c

--- principal_deck_save.c
+++ principal_deck_save.c
@@ -1,19 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "include.h"
 
-void principale_deck_save(FILE *fichier, FILE *register_zone, int number_of_cards) {
+void principale_deck_save(FILE *fichier, int number_of_cards) {
+    FILE* register_zone = fopen("MainDeckSave.txt", "w+");  
+    if (register_zone == NULL) {
+        printf("Error opening MainDeckSave.txt\n");
+        return;
+    }
+
     int *principal_deck = calloc(number_of_cards, sizeof(int));
     if (principal_deck == NULL) {
         printf("Memory allocation error\n");
+        fclose(register_zone);
         return;
     }
-    int i = 0, a;
+
+    int i = 0;
     rewind(fichier);
     while (fscanf(fichier, "%d", &a) == 1 && i < number_of_cards) {
-        principal_deck[i] = a;
         fprintf(register_zone, "%d\n", principal_deck[i]);
         i++;
     }
 
     free(principal_deck);
+    fclose(register_zone);
 }
