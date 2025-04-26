47d117217f46441ca936182d05655d1b03f51605
Kkirbo <aymaneamara935@gmail.com>
2025-04-26 20:30:37 +0200

    Update include.h

--- include.h
+++ include.h
@@ -9,3 +9,8 @@ int IsPrimeNumber(int nombre);
 int sizeNumber(int number);
 
 FILE* cards_atribution();
+
+void principale_deck_save(FILE* f, int nb_cards);
+void SaveDiscardPile(Player* p, int nb_players);
+void demandeur_sauvegarde(FILE* f, int nb_cards, Player* players, int nb_players);
+
