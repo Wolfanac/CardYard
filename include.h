77988290997a6e7bb620cb9018e8558c51004c99
Kkirbo <aymaneamara935@gmail.com>
2025-05-05 11:03:39 +0200

    Update include.h

--- include.h
+++ include.h
@@ -2,13 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#if defined(_WIN32) || defined(_WIN64)
-    #include <conio.h>
-    #include <windows.h>
-#else
-    #include <termios.h>
-    #include <unistd.h>
-#endif
+
 
 int IsPrimeNumber(int nombre);
 
