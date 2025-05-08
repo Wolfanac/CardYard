cbe19da26619ebb4ffea0df6dd1268f736eb0eb5
Silvric <delaplaceric@gmail.com>
2025-05-09 00:36:37 +0200

    ending.c

--- ending.c
+++ ending.c
@@ -15,14 +15,14 @@ int checkEnd(Player* p){
     return 1;
 }
 //sort the results
-void sortArray(int result[], int nb_player) {
+void sortArray(int scoresresult[], int nb_player) {
     int temp;
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+    for (int i = 0; i < nb_player - 1; i++) {
+        for (int j = 0; j < nb_player - i - 1; j++) {
+            if (scoresresult[j] > scoresresult[j + 1]) {
+                temp = scoresresult[j];
+                scoresresult[j] = scoresresult[j + 1];
+                scoresresult[j + 1] = temp;
             }
         }
     }
@@ -57,16 +57,16 @@ void endgame(Player** game, int nb_player, int row, int col, int max){
     }
 
     int min=result[0];
-    int scoresresult[nb_player];
+    int scoresresult[nb_player], index[nb_player];
     for (int i=0; i<nb_player; i++){
        sortArray(scoresresult, nb_player);
        }
-    int minIndex=[8];
+    int minIndex=0;
     int equal=0, minimum=0, a=0;
     for (int i=1; i<nb_player; i++){
         if (result[i]<min){
             min=result[i];
-            minIndex[0]=i;
+            minIndex=i;
         }
     }
     for (int i=1; i<nb_player; i++){
@@ -78,20 +78,20 @@ void endgame(Player** game, int nb_player, int row, int col, int max){
       
     for (int i=1; i<nb_player; i++){
       if (scoresresult[i]==minimum){
-        minIndex[a]=i;
+        index[a]=i;
         a++;
         }
       }
     
     if (equal==1&&min==equal){
       printf("\nthere is an equality for %d points ! the top players are :\n", min);
-      for (int b=a, b<=0; b--){
-        printf("player numbered %d named %s",minIndex[b]+1, game[minIndex[b]]->nickname);
+      for (int b=a; b<=0; b--){
+        printf("player numbered %d named %s",index[b]+1, game[index[b]]->nickname);
         }
         
     }
     else{
-      printf("\nThe winner of the game is the player number %d, with has    the lowest total of points with %d points, which means %s won!", minIndex[0]+1, min, game[minIndex[0]]->nickname);
+      printf("\nThe winner of the game is the player number %d, with has    the lowest total of points with %d points, which means %s won!", minIndex+1, min, game[minIndex]->nickname);
     }
     free(result);
 }
